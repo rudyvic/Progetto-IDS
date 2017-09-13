17cbf58f258e8a78595923371bcb8856768faf1d
Rudy Vicario <visotruce@gmail.com>
2017-09-13 21:13:49 +0200

    d

---
+++ Progetto IDS/src/Model/ModelCatalog.java
@@ -0,0 +1,86 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Observable;
+
+import Model.Catalog;
+import Model.Disc;
+
+public class ModelCatalog extends Observable{
+	
+	private DatabaseQuery database = DatabaseQuery.getInstance();
+	private Catalog catalog;
+	
+	public ModelCatalog(){
+		
+	}
+	
+	public double getMaxPrice(){
+		return database.getMaxPrice();
+	}
+	
+	public List<Disc> getCatalogRunningOut() {
+		synchCatalog();
+		
+		List<Disc> cat = new ArrayList<Disc>();
+		
+		for(Map.Entry<Disc,Integer> entry : catalog.catalog.entrySet()) {
+		    Disc disc = entry.getKey();
+		    int quantity = entry.getValue();
+		    
+		    if(quantity<=2) {
+		    	cat.add(disc);
+		    }
+		}
+		
+		return cat;
+	}
+	
+	public Catalog buy(Catalog cat, Buy b) {
+		Catalog bought = new Catalog();
+
+		b = database.buyCD(b);
+		
+		for(Disc disc : cat.getCatalog()) {
+			int quantityInStore = this.catalog.getQuantity(disc);
+			int quantityToBuy = cat.getQuantity(disc);
+			
+			if(quantityToBuy <= quantityInStore) {
+				this.editDiscQuantity(disc, quantityInStore-quantityToBuy);
+				database.boughtCD(b.getCode(), disc.getCode());
+				bought.add(disc, quantityToBuy);
+			}
+		}
+		
+		return bought;
+	}
+	
+	public Catalog synchCatalog() {
+		this.catalog = database.getCatalog();
+		return this.catalog;
+	}
+	
+	public List<Disc> searchCatalog(String title, String genre, Double minPrice, Double maxPrice){
+		synchCatalog();
+		Catalog c = new Catalog();
+		c = database.searchCatalog(title, genre, minPrice, maxPrice);
+		return new ArrayList<Disc>(c.catalog.keySet());
+	}
+
+	public void add(Disc disc, int quantity) {
+		database.addDisc(disc, quantity);
+		synchCatalog();
+	}
+	
+	public void remove(Disc disc) {
+		database.removeFromStore(disc.getCode());
+		synchCatalog();
+	}
+
+	public void editDiscQuantity(Disc disc, int quantity) {
+		database.editStoreQuantity(disc.getCode(),quantity);
+		synchCatalog();
+	}
+}
